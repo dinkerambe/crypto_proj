aa74c70b3c29f04fefb81d99e047b9562f50bd2c
szupie <szupie@TARDIS.local>
2014-03-31 11:49:54 -0400

    class structure

--- speck.cpp
+++ speck.cpp
@@ -2,3 +2,17 @@
 #include "uberzahl.h"
 using namespace std;
 
+class Speck {
+  public:
+    Speck();
+    void setKey(uberzahl);
+    uberzahl encrypt(uberzahl);
+    uberzahl decrypt(uberzahl);
+  private:
+    uberzahl key;
+    uberzahl * keywords;
+};
+
+int main() {
+  uberzahl x = 13;
+}
