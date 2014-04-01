33d28ff856eac4734aecf90fc47ffa3733447825
Dinker Ambe <dinkerambe@Dinkers-MacBook-Pro.local>
2014-04-01 14:52:34 -0400

    chagned rotate to expand

--- speck.cpp
+++ speck.cpp
@@ -14,7 +14,7 @@ void Speck::genKey() {
 	//cout << key << endl;
 }
 
-void Speck::rotate(uberzahl x, uberzahl y, uberzahl k){
+void Speck::expand(uberzahl x, uberzahl y, uberzahl k){
 	x = x.rotateRight(ALPHA,0,WORDSIZE-1);
 	x = x + y;
 	x = x ^ k;
